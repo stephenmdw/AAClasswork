f6095bad96dd6bbab4682b1967368fefa1b5f7a8
connorryanbaker <connorbaker@gmail.com>
2019-06-27 10:56:03 -0400

    add before hook to my_reverse

--- specs/my_reverse_spec.rb
+++ specs/my_reverse_spec.rb
@@ -1,13 +1,13 @@
 describe "my_reverse" do
   let(:array) { [ "a", "b", "c", "d" ] }
 
-  it "should NOT use built-in ruby #reverse method" do
+  before(:each) "should NOT use built-in ruby #reverse method" do
     expect(array).not_to receive(:reverse!)
     expect(array).not_to receive(:reverse)
     array.my_reverse
   end
 
   it "Reverses an array" do
-    expect(array.my_reverse).to eq(array.reverse)
+    expect(array.my_reverse).to eq(["d", "c", "b", "a"])
   end
 end
