5f957cd7d02a013f09adad612e17a0719a1f661d
connorryanbaker <connorbaker@gmail.com>
2019-07-16 10:30:43 -0400

    update jumble sort spec/description

--- specs/jumble_sort_spec.rb
+++ specs/jumble_sort_spec.rb
@@ -1,11 +1,21 @@
 # Write a method that takes a string and an alphabet. It returns a copy of the string
 # with the letters re-ordered according to their positions in the alphabet. If
 # no alphabet is passed in, it defaults to normal alphabetical order (a-z).
+# **Do NOT use `Array#sort` or `Array#sort_by`
 
 # Example:
 # `jumble_sort("hello")` => "ehllo"
 # `jumble_sort("hello", ['o', 'l', 'h', 'e'])` => 'ollhe'
 describe "#jumble_sort" do
+
+  before(:each) do 
+    expect_any_instance_of(String).to not_receive(:sort)
+    expect_any_instance_of(String).to not_receive(:sort!)
+    expect_any_instance_of(String).to not_receive(:sort_by)
+    expect_any_instance_of(String).to not_receive(:sort_by!)
+    jumble_sort("lol")
+  end
+
   it "defaults to alphabetical order" do
     expect(jumble_sort("hello")).to eq("ehllo")
   end
