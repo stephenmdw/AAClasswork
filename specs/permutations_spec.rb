11575594244edb9364b7a74b9163da59be7ce0cb
ronilbhatia <ronilbhatia@gmail.com>
2019-08-13 14:35:16 -0700

    Clean up permutations spec

--- specs/permutations_spec.rb
+++ specs/permutations_spec.rb
@@ -1,14 +1,20 @@
 describe "permutations" do
   let(:arr) { [1, 2, 3] }
+  let(:perms) { [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] }
 
   before(:each) do
-    arr = [1, 2, 3, [4, [5, 6]], [[[7]], 8]]
-    expect(arr).not_to receive(:permutation)
+    expect_any_instance_of(Array).not_to receive(:permutation)
   end
 
+  it "works for an empty array" do
+    expect(permutations([])).to match_array([[]])
+  end
+
+  it "works for an array of one item" do
+    expect(permutations([1])).to match_array([[1]])
+  end
+  
   it "returns all permutations of an array" do
-    perms_arr = arr.permutation.to_a
-    
-    expect(permutations(arr)).to match_array(perms_arr)
+    expect(permutations(arr)).to match_array(perms)
   end
 end
